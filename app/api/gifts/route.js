bfbd3ddc45b721334c7a57b1c4989474c0ab2f72
Antonio <antonioryarbough@gmail.com>
2026-02-19 11:57:39 -0500

    Fix import path for gifts data in API route

--- app/api/gifts/route.js
+++ app/api/gifts/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import gifts from '../../data/gifts.json';
+import gifts from '../../../data/gifts.json';
 
 export async function GET() {
   return NextResponse.json(gifts);
