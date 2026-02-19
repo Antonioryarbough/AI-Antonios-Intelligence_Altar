de796a6612598a6ecc59a71371eeff5f22efd8ca
Antonio <antonioryarbough@gmail.com>
2026-02-19 01:23:08 -0500

    Add Firebase Admin and Stripe SDKs for send-gift API routes

--- app/api/_stripe.js
+++
@@ -1,8 +0,0 @@
-// Stripe singleton for API routes
-import Stripe from 'stripe';
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
-});
-
-export default stripe;
